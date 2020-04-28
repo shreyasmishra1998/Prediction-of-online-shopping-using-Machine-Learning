58a12a245f0dbce407d8b3043ea88564c1552707
shreyasmishra1998 <40026668+shreyasmishra1998@users.noreply.github.com>
2020-04-28 12:21:54 +0530

    Add files via upload

---
+++ shopping.py
@@ -0,0 +1,165 @@
+import csv
+import sys
+import csv
+
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix
+
+model = KNeighborsClassifier(n_neighbors=1)
+
+TEST_SIZE = 0.4
+
+def main():
+
+    # Check command-line arguments
+    if len(sys.argv) != 2:
+        sys.exit("Usage: python shopping.py data")
+    # Load data from spreadsheet and split into train and test sets
+    evidence, labels = load_data(sys.argv[1])
+    global y_test
+    X_train, X_test, y_train, y_test = train_test_split(
+        evidence, labels, test_size=TEST_SIZE
+    )
+
+    # Train model and make predictions
+    model = train_model(X_train, y_train)
+    predictions = model.predict(X_test)
+    sensitivity, specificity = evaluate(y_test, predictions)
+
+    # Print results
+    print(f"Correct: {(y_test == predictions).sum()}")
+    print(f"Incorrect: {(y_test != predictions).sum()}")
+    print(f"True Positive Rate: {100 * sensitivity:.2f}%")
+    print(f"True Negative Rate: {100 * specificity:.2f}%")
+
+
+def load_data(filename):
+    """
+    Load shopping data from a CSV file `filename` and convert into a list of
+    evidence lists and a list of labels. Return a tuple (evidence, labels).
+
+    evidence should be a list of lists, where each list contains the
+    following values, in order:
+        - Administrative, an integer
+        - Administrative_Duration, a floating point number
+        - Informational, an integer
+        - Informational_Duration, a floating point number
+        - ProductRelated, an integer
+        - ProductRelated_Duration, a floating point number
+        - BounceRates, a floating point number
+        - ExitRates, a floating point number
+        - PageValues, a floating point number
+        - SpecialDay, a floating point number
+        - Month, an index from 0 (January) to 11 (December)
+        - OperatingSystems, an integer
+        - Browser, an integer
+        - Region, an integer
+        - TrafficType, an integer
+        - VisitorType, an integer 0 (not returning) or 1 (returning)
+        - Weekend, an integer 0 (if false) or 1 (if true)
+
+    labels should be the corresponding list of labels, where each label
+    is 1 if Revenue is true, and 0 otherwise.
+    """
+    with open("shopping.csv") as f:
+        reader = csv.reader(f)
+        next(reader)
+        evidence = []
+        label = []
+        month = []
+        for row in reader:
+            a = row
+            i = 0
+            data = []
+            while(i<len(a)):
+                if(i in (1,3,5,6,7,8,9)):
+                    data.append(float(a[i]))
+                if(i in (0,2,4,11,12,13,14)):
+                    data.append(int(a[i]))
+                if(i==10):
+                    if(a[i]=='Jan'):
+                        data.append(0)
+                    if(a[i]=='Feb'):
+                        data.append(1)
+                    if(a[i]=='Mar'):
+                        data.append(2)
+                    if(a[i]=='Apr'):
+                        data.append(3)
+                    if(a[i]=='May'):
+                        data.append(4)
+                    if(a[i]=='June'):
+                        data.append(5)
+                    if(a[i]=='Jul'):
+                        data.append(6)
+                    if(a[i]=='Aug'):
+                        data.append(7)
+                    if(a[i]=='Sep'):
+                        data.append(8)
+                    if(a[i]=='Oct'):
+                        data.append(9)
+                    if(a[i]=='Nov'):
+                        data.append(10)
+                    if(a[i]=='Dec'):
+                        data.append(11)
+                if(i==15):
+                    if(a[i]=='Returning_Visitor'):
+                        data.append(1)
+                    else:
+                        data.append(0)
+                if(i==16):
+                    if(a[i]=='FALSE'):
+                        data.append(0)
+                    else:
+                        data.append(1)
+                if(i==17):
+                    label.append(0 if row[17] == "FALSE" else 1)
+                i = i + 1
+            evidence.append(data)
+    return(evidence,label)
+
+    raise NotImplementedError
+
+
+def train_model(evidence, labels):
+    """
+    Given a list of evidence lists and a list of labels, return a
+    fitted k-nearest neighbor model (k=1) trained on the data.
+
+    """
+    X_train, X_test, y_train, y_test = train_test_split(
+        evidence, labels, test_size=TEST_SIZE
+    )
+    return(model.fit(X_train, y_train))
+    
+    raise NotImplementedError
+
+
+def evaluate(labels, predictions):
+    """
+    Given a list of actual labels and a list of predicted labels,
+    return a tuple (sensitivity, specificty).
+
+    Assume each label is either a 1 (positive) or 0 (negative).
+
+    `sensitivity` should be a floating-point value from 0 to 1
+    representing the "true positive rate": the proportion of
+    actual positive labels that were accurately identified.
+
+    `specificity` should be a floating-point value from 0 to 1
+    representing the "true negative rate": the proportion of
+    actual negative labels that were accurately identified.
+    """
+    cm = confusion_matrix(y_test,predictions)
+    TP = cm[1][1]
+    TN = cm[0][0]
+    FP = cm[0][1]
+    FN = cm[1][0]
+    sensitivity = (TP / float(TP + FN))
+    specificity = (TN / float(TN + FP))
+    return({sensitivity,specificity})
+    raise NotImplementedError
+
+
+if __name__ == "__main__":
+    main()
